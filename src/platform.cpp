ba38e23599df3d7c6359fad2fc5d080a15208503
Gemba <uid0@sdf-eu.org>
2026-04-25 08:30:20 +0200

    added hash for updated platforms map

--- src/platform.cpp
+++ src/platform.cpp
@@ -301,7 +301,8 @@ int Platform::isPlatformCfgfilePristine(const QString &cfgFilePath) {
                  "bf12d0f2f7161d45041f8996c44d6c3c2f666cfc33938dbcbd506c1f766062c4",
                  "44a416856327c01c1ec73c41252f9c3318bf703c33fd717935f31b37e635f413",
                  "9af2abea78af7b94b8c86d97417fb4aff347a8b6eef5c0fdab37be31938f5f9a",
-                 "0c4a1cb2cde6c772c3125d59a1ae776a0cc05888520f131b3e058fbb91be00dd"}
+                 "0c4a1cb2cde6c772c3125d59a1ae776a0cc05888520f131b3e058fbb91be00dd",
+                 "17d70390fdc1b6f2bb316087227a307a7190c0de00d88308507698163ef0b0a4"}
             )
         }
         // clang-format on
